25f57c707a61f25f0d8ce8d2988bf59ac24430e4
emaibarra <ibarra.emanuel.022@gmail.com>
2024-10-13 16:15:41 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,61 @@
+//punto 1
+{
+//punto 1.2
+//let a = 3;
+//let b = 4;
+//let c = a+b;
+
+//console.log("La suma de", a ,"+", b ,"es", c);
+
+//punto 1.3
+//let nombrePromt = prompt("Cual es tu nombre?");
+//console.log("Saludos, ",nombrePromt);
+}
+
+//punto 2
+/*{
+//punto 2.1
+let a = 3;
+let b = 7;
+console.log(a);
+if(a>b){
+    console.log("el mayor es", a);
+} else 
+console.log("el mayor es", b);
+
+//punto 2.2
+let numeroIngresado = prompt("ingresa un numero")
+if(numeroIngresado%2==0){
+    console.log(numeroIngresado, "es par")
+} else
+console.log(numeroIngresado, "es impar")
+}
+*/
+
+/*//punto 3
+{
+//punto 3.1
+for(let a = 10; a>=0; a--) {
+    console.log(a);
+}
+
+//punto 3.2
+let ingreso;
+do{
+ingreso = prompt("ingrese un numero mas grande que 100");
+} while(ingreso<=100)
+console.log("ingresaste un numero mas grande que 100");
+}*/
+
+{
+let esPar=(num) =>{
+
+    if(num%2==0){
+        console.log("el numero ",num, "es par", true)
+    } else
+    console.log("el numero ",num, "es impar", false)
+    }
+}
+
+
+
